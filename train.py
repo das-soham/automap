49942a7625437ad989fb493b193fdbc8b8402aa7
Soham Das <sohamdas@gmail.com>
2026-04-02 23:44:41 +0530

    Modification 2.01

--- train.py
+++ train.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
         print("CREATING GPR COMPOSITE")
         print("="*70)
         composite = create_gpr_composite(
-            data_file=args.data,
+            data_file='data.parquet',
             start_date=args.start_date,
             end_date=args.end_date,
             output_file=args.gpr_output
